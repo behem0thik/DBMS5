c80670da4af72f4f1304fc3d2f236aba0de5d4c1
behem0thik <behem0thik@users.noreply.github.com>
2022-12-14 16:54:32 +0600

    fix schema-init.sql

--- src/main/resources/schema-init.sql
+++ src/main/resources/schema-init.sql
@@ -5,6 +5,7 @@ CREATE TABLE public.users
 (
     user_id    VARCHAR(255) PRIMARY KEY            NOT NULL,
     username   VARCHAR(255)                        NOT NULL,
+    password   VARCHAR(255)                        NOT NULL,
     created_at TIMESTAMP DEFAULT current_timestamp NOT NULL,
     updated_at TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
@@ -13,12 +14,12 @@ CREATE TABLE public.posts
 (
     post_id       VARCHAR(255) PRIMARY KEY            NOT NULL,
     user_id       VARCHAR(255)                        NOT NULL,
-    article       VARCHAR(255)                        NOT NULL,
-    post_content  VARCHAR(255)                        NOT NULL,
+    article       VARCHAR(100)                        NOT NULL,
+    post_content  BYTEA                               NOT NULL,
     image         BYTEA                               NOT NULL,
+    post_category VARCHAR(255)                        NOT NULL,
     created_at    TIMESTAMP DEFAULT current_timestamp NOT NULL,
     updated_at    TIMESTAMP DEFAULT current_timestamp NOT NULL,
-    post_category VARCHAR                             NOT NULL,
     CONSTRAINT FK_posts_users FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
