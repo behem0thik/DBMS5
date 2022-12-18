cb02af575ce3764b674ae3f70ea356c7a8ce2501
behem0thik <behem0thik@users.noreply.github.com>
2022-12-18 20:30:03 +0600

    add Tag tables

--- src/main/resources/schema-init.sql
+++ src/main/resources/schema-init.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS public.posts_tags;
 DROP TABLE IF EXISTS public.posts;
 DROP TABLE IF EXISTS public.users;
+DROP TABLE IF EXISTS public.tags;
 
 CREATE TABLE public.users
 (
@@ -17,11 +19,22 @@ CREATE TABLE public.posts
     article       VARCHAR(100)                        NOT NULL,
     post_content  BYTEA                               NOT NULL,
     image         BYTEA                               NOT NULL,
-    post_category VARCHAR(255)                        NOT NULL,
     created_at    TIMESTAMP DEFAULT current_timestamp NOT NULL,
     updated_at    TIMESTAMP DEFAULT current_timestamp NOT NULL,
     CONSTRAINT FK_posts_users FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
+CREATE TABLE public.tags
+(
+    tag_id   VARCHAR(255) PRIMARY KEY NOT NULL,
+    tag_name VARCHAR(255)             NOT NULL
+);
 
+CREATE TABLE public.posts_tags
+(
+    post_id VARCHAR(255) PRIMARY KEY NOT NULL,
+    tag_id  VARCHAR(255)             NOT NULL,
+    CONSTRAINT FK1_posts_tags FOREIGN KEY (post_id) REFERENCES posts (post_id),
+    CONSTRAINT FK2_posts_tags FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
+)
 
