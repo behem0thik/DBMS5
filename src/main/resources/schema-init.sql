84e0c1715ec7a468b3a15a9d417be1ac1f4eceb6
behem0thik <behem0thik@users.noreply.github.com>
2022-12-26 00:06:52 +0600

    refactor post_content type and keys

--- src/main/resources/schema-init.sql
+++ src/main/resources/schema-init.sql
@@ -17,8 +17,8 @@ CREATE TABLE public.posts
     post_id      VARCHAR(255) PRIMARY KEY            NOT NULL,
     user_id      VARCHAR(255)                        NOT NULL,
     article      VARCHAR(100)                        NOT NULL,
-    post_content VARCHAR(255)                        NOT NULL,
-    image        BYTEA                               NOT NULL,
+    post_content OID                                 NOT NULL,
+    image        BYTEA                               NULL,
     created_at   TIMESTAMP DEFAULT current_timestamp NOT NULL,
     updated_at   TIMESTAMP DEFAULT current_timestamp NOT NULL,
     CONSTRAINT FK_posts_users FOREIGN KEY (user_id) REFERENCES users (user_id)
@@ -32,9 +32,9 @@ CREATE TABLE public.tags
 
 CREATE TABLE public.posts_tags
 (
-    post_id VARCHAR(255) PRIMARY KEY NOT NULL,
-    tag_id  VARCHAR(255)             NOT NULL,
+    tag_id  VARCHAR(255) NOT NULL,
+    post_id VARCHAR(255) NOT NULL,
     CONSTRAINT FK1_posts_tags FOREIGN KEY (post_id) REFERENCES posts (post_id),
     CONSTRAINT FK2_posts_tags FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
-)
+);
 
