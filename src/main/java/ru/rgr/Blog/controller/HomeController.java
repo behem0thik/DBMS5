68134f5103f5e6599ba8da4ccffe656a6dde366b
behem0thik <behem0thik@users.noreply.github.com>
2022-12-26 00:52:51 +0600

    add read and create methods to model Post

---
+++ src/main/java/ru/rgr/Blog/controller/HomeController.java
@@ -0,0 +1,66 @@
+package ru.rgr.Blog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.rgr.Blog.model.Post;
+import ru.rgr.Blog.model.User;
+import ru.rgr.Blog.service.PostService;
+import ru.rgr.Blog.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/")
+@Slf4j
+public class HomeController {
+    private final PostService postService;
+    private final UserService userService;
+
+    @Autowired
+    public HomeController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String findFivePosts(Model model) {
+        List<Post> posts = postService.findAll().stream().limit(5).toList();
+
+//        log.info("posts : {}", posts);
+        model.addAttribute("posts", posts);
+        log.info("model : {}", model);
+        return "/main";
+    }
+
+    @GetMapping("/post-create")
+    public String createPostForm(Model model) {
+
+
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "/post-create";
+    }
+
+    @PostMapping("/post-create")
+    public String createPost(Post post) {
+        User user = userService.findUserByUsername("Valera");
+        post.setUserId(user.getUserId());
+        // TODO: 25.12.2022 костыль, пока нет spring security
+
+        log.info("create new post: {}", post);
+        postService.create(post);
+        return "redirect:/";
+    }
+
+    @PostMapping("/post-delete/{id}")
+    public void deletePost(@PathVariable("id") UUID postId) {
+        postService.deleteById(postId);
+    }
+}
