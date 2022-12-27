b3bf6c289cbd8591cb7def91270e98f5d1dd645b
behem0thik <behem0thik@users.noreply.github.com>
2022-12-27 19:48:00 +0600

    move all urls with posts to separate class

---
+++ src/main/java/ru/rgr/Blog/controller/PostController.java
@@ -0,0 +1,93 @@
+package ru.rgr.Blog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.rgr.Blog.model.Post;
+import ru.rgr.Blog.model.User;
+import ru.rgr.Blog.service.PostService;
+import ru.rgr.Blog.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/posts")
+@Slf4j
+public class PostController {
+    private final PostService postService;
+    private final UserService userService;
+
+    @Autowired
+    public PostController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String findPosts(Model model) {
+        List<Post> posts = postService.findAll();
+
+        model.addAttribute("posts", posts);
+        log.info("Got all posts");
+        return "/posts";
+    }
+
+    @GetMapping("/create")
+    public String createPostForm(Model model) {
+        Post post = new Post();
+
+        model.addAttribute("post", post);
+        log.info("post creation form");
+        return "post-create";
+    }
+
+    @PostMapping("/create")
+    public String createPost(Post post) {
+        // TODO: 25.12.2022 костыль, пока нет spring security
+        User user = userService.findUserByUsername("Valera");
+        post.setUserId(user.getUserId());
+
+        postService.create(post);
+        log.info("Create new post with given id: {}", post.getPostId());
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updatePostForm(@PathVariable UUID id, Model model) {
+        Post post = postService.findPostById(id);
+
+        model.addAttribute("post", post);
+        log.info("update form to post with id");
+        return "post-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updatePost(Post post, @PathVariable UUID id) {
+        // TODO: 25.12.2022 костыль, пока нет spring security
+        User user = userService.findUserByUsername("Valera");
+        post.setUserId(user.getUserId());
+
+        postService.update(id, post);
+        log.info("Updated post with given id: {}", post.getPostId());
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/{id}")
+    public String deletePost(@PathVariable("id") UUID postId) {
+        postService.deleteById(postId);
+        log.info("Delete post with given id: {}", postId);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/{id}")
+    public String readMore(@PathVariable("id") UUID postId, Model model) {
+        Post post = postService.findPostById(postId);
+
+        log.info("Got post with given id: {}", post.getPostId());
+        model.addAttribute("post", post);
+        return "post";
+    }
+}
